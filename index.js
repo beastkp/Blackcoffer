3f5e3bd0afce72418a4c9895bd023cff8ae97619
beastkp <kroshpan@gmail.com>
2024-03-12 11:38:49 +0530

    Production issues

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
 const connectDB = require("./db/connect");
 const cors = require("cors");
 require("dotenv").config();
 
 const insertRouter = require("./routes/insertion");
-const dashboardRouter =require('./routes/dashboard')
+const dashboardRouter = require("./routes/dashboard");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: [
+          "'self'",
+          "http://127.0.0.1:8000",
+          "https://resource-insights.onrender.com/",    
+        ],
+      },
+    },
+  })
+);
 
+app.use("/api/v1/json", insertRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
-app.use('/api/v1/json',insertRouter);
-app.use('/api/v1/dashboard',dashboardRouter);
-
-
-const port  = 8080;
-const start = async ()=>{
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port,()=>{
-            console.log(`Server is listening on port ${port}`)
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+const port = 8080;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
-
-
