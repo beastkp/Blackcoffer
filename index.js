1ad4c361310ae7ed0a6fd7b360f5d5783bb172c7
beastkp <kroshpan@gmail.com>
2024-03-21 19:49:44 +0530

    removed helmet

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ const dashboardRouter = require("./routes/dashboard");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        connectSrc: [
-          "'self'",
-          "http://127.0.0.1:8000",
-          "ws://localhost:42877/",
-        ],
-      },
-    },
-  })
-);
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: {
+//       directives: {
+//         defaultSrc: ["'self'"],
+//         connectSrc: [
+//           "'self'",
+//           "http://127.0.0.1:8000",
+//           "ws://localhost:42877/",
+//         ],
+//       },
+//     },
+//   })
+// );
 
 app.use("/api/v1/json", insertRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
